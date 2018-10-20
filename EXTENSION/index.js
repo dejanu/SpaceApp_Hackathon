7cb6293a48631494bd9cb1604cdecabd56a35443
eugenius <eugen.cernei@gmail.com>
2018-10-20 17:10:05 +0300

    Fixed edlement loading.

--- EXTENSION/index.js
+++ EXTENSION/index.js
@@ -17,20 +17,14 @@ function main() {
 function setupHTMLContent(jsonArray) {
     const contentElement = document.getElementById("content")
     for (const index in jsonArray) {
-        contentElement.add(generateCardItem(jsonArray[index]))
+        contentElement.append(generateCardItem(jsonArray[index]))
     }
 }
-
-// {/* <div class="card border-primary mb-3" style="max-width: 18rem;">
-//       <div class="card-header">Header</div>
-//       <div class="card-body text-primary">
-//         <h5 class="card-title">Primary card title</h5>
-//         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//       </div> */}
 
 function generateCardItem(item) {
     console.log(item)
     const divElement = document.createElement("div")
+    divElement.textContent = item.title
     return divElement
 }
 
