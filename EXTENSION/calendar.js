795ff74f0febb44dd6c5c64b935a0cca1d5fc07a
eugenius <eugen.cernei@gmail.com>
2018-10-21 13:10:18 +0300

    Auth works, calendar doesn't.

---
+++ EXTENSION/calendar.js
@@ -0,0 +1,45 @@
+window.addEventListener("load", (event) => {
+    authorize()
+}, false)
+
+var bearerToken = ""
+
+var event = {
+    'summary': 'Google I/O 2015',
+    'location': '800 Howard St., San Francisco, CA 94103',
+    'description': 'A chance to hear more about Google\'s developer products.',
+    'start': {
+      'dateTime': '2015-05-28T09:00:00-07:00',
+      'timeZone': 'America/Los_Angeles'
+    },
+    'end': {
+      'dateTime': '2015-05-28T17:00:00-07:00',
+      'timeZone': 'America/Los_Angeles'
+    },
+    'recurrence': [
+      'RRULE:FREQ=DAILY;COUNT=2'
+    ],
+    'attendees': [
+      {'email': 'lpage@example.com'},
+      {'email': 'sbrin@example.com'}
+    ],
+    'reminders': {
+      'useDefault': false,
+      'overrides': [
+        {'method': 'email', 'minutes': 24 * 60},
+        {'method': 'popup', 'minutes': 10}
+      ]
+    }
+  };
+
+
+function authorize() {
+    chrome.identity.getAuthToken({interactive: true},(token)=>{
+        bearerToken = "Bearer " + token    
+    })
+    
+}
+
+function createEvent() {
+    console.log(chrome.calendar)
+}
